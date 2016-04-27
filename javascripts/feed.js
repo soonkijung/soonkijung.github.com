d408a146e43e382a3c7dc00b71826a7cd6af4826
soonkijung <soonki.jung@gmail.com>
2016-04-28 00:41:34 +0900

    Update feed.js

--- javascripts/feed.js
+++ javascripts/feed.js
@@ -1,4 +1,4 @@
-$url="kuptf.org/?feed=rss2&page_id=70"
+$url="http://kuptf.org/?feed=rss2&page_id=70"
 $rss = simplexml_load_file("$url");
 $content = $rss->channel->item->children( "http://kuptf.org/?page_id=70" );
 foreach ($rss->channel->item as $item) {
